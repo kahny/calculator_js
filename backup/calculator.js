7cfe718adcedf318a7a875e31a715780dce0fc24
Connie Chang <changchilla@gmail.com>
2014-07-11 09:43:06 -0700

    added +=

--- backup/calculator.js
+++ backup/calculator.js
@@ -2,18 +2,19 @@
 
 window.onload = function(event){
 
-	result = []  //http://stackoverflow.com/questions/17583544/referenceerror-variable-is-not-defined   this is why you don't declare var inside
+	var result = []  //http://stackoverflow.com/questions/17583544/referenceerror-variable-is-not-defined   this is why you don't declare var inside
 	
+	var idArray = ["zero", "one","two", "three", "four", "five", "six", "seven", "eight", "nine"];
 	//The following variables get the values of the number keys 
-	var one = document.getElementById("one"),
-		two = document.getElementById("two"),
-		three = document.getElementById("three"),
-		four = document.getElementById("four"),
-		five = document.getElementById("five"),
-		six = document.getElementById("six"),
-		seven = document.getElementById("seven"),
-		eight = document.getElementById("eight"),
-		nine = document.getElementById("nine");
+	
+
+	// Set variables for the corresponding number div, for instance "var one = document.getElementById("one")""
+	for(var i=0; i<idArray.length; i++){
+		var id = idArray[i];	
+		id = document.getElementById(id)
+
+	}	
+
 
 	//The following variables are the operator keys 
 	var multiply = document.getElementById("multiply"),
@@ -24,61 +25,95 @@ window.onload = function(event){
 		clear = document.getElementById('clear')
 
 
-	//number keys
+	/*set onclick functions for each key, for instance:
+
 	one.onclick = function(event){
-		document.getElementById("result_display_value").innerHTML += 1	
+		document.getElementById("result_display_value").innerHTML = 1	
+		result.push(1)
+		console.log(result)
+	}
+	*/
+
+
+	var idArray2 = {"zero":0, "one":1,"two":2, "three":3, "four":4, "five":5, "six":6, "seven":7, "eight":8, "nine":9};
+	
+	for(var i = 0; i < idArray.length; i++){
+		var id = idArray[i];  // one, two etc.
+		// console.log(id)
+	 	var idNum = idArray2[id] //1, 2 etc.
+		// console.log(idNum)
+		
+
+		//This test doesn't work 
+		// one.onclick= function(event){
+		// 	console.log("HI")
+		// }
+
+		id.onclick = function(event){
+			document.getElementById("result_display_value").innerHTML = idNum
+			result.push(idNum)
+			console.log(idNum)
+			console.log(result)
+			console.log("HI!")
+		}
+
+	}
+/*
+
+	one.onclick = function(event){
+		document.getElementById("result_display_value").innerHTML = 1	
 		result.push(1)
 		console.log(result)
 	}
 
 	two.onclick = function(event){
-		document.getElementById("result_display_value").innerHTML += 2 
+		document.getElementById("result_display_value").innerHTML = 2 
 		result.push(2)
 		console.log(result)
 	}
 
 	three.onclick = function(event){
-		document.getElementById("result_display_value").innerHTML += 3 
+		document.getElementById("result_display_value").innerHTML = 3 
 		result.push(3)
 		console.log(result)
 	}
 
 	four.onclick = function(event){
-		document.getElementById("result_display_value").innerHTML += 4 
+		document.getElementById("result_display_value").innerHTML = 4 
 		result.push(4)
 		console.log(result)
 	}
 
 	five.onclick = function(event){
-		document.getElementById("result_display_value").innerHTML += 5 
+		document.getElementById("result_display_value").innerHTML = 5 
 		result.push(5)
 		console.log(result)
 	}
 
 	six.onclick = function(event){
-		document.getElementById("result_display_value").innerHTML += 6 
+		document.getElementById("result_display_value").innerHTML = 6 
 		result.push(6)
 		console.log(result)
 	}
 
 	seven.onclick = function(event){
-		document.getElementById("result_display_value").innerHTML += 7 
+		document.getElementById("result_display_value").innerHTML = 7 
 		result.push(7)
 		console.log(result)
 	}
 
 	eight.onclick = function(event){
-		document.getElementById("result_display_value").innerHTML += 8 
+		document.getElementById("result_display_value").innerHTML = 8 
 		result.push(8)
 		console.log(result)
 	}
 
 	nine.onclick = function(event){
-		document.getElementById("result_display_value").innerHTML += 6 
+		document.getElementById("result_display_value").innerHTML = 6 
 		result.push(6)
 		console.log(result)
 	}
-
+*/
 
 	//OPERATORS 
 	multiply.onclick = function(event){
@@ -117,7 +152,7 @@ window.onload = function(event){
 
 	clear.onclick = function(event){
 		result = [];
-		document.getElementById("result_display_value").innerHTML = "0"
+		document.getElementById("result_display_value").innerHTML = ""
 		document.getElementById("operator_display").innerHTML = ""
 	}
 
